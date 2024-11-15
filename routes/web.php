66c9ea70f5e2eccfd8ddec3d134e4f480a483a42
Anam-jafar <anamibnjafar@gmail.com>
2024-11-15 06:16:22 +0000

    routes modify

--- routes/web.php
+++ routes/web.php
@@ -10,52 +10,52 @@ use App\Http\Controllers\CollectionController;
 
 
 
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/generic/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/generic/register', [AuthController::class, 'register']);
+Route::get('/generic/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/generic/login', [AuthController::class, 'login']);
+Route::post('/generic/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
 
     // Profile URL
 
-    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/generic/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/generic/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/generic/profile', [ProfileController::class, 'update'])->name('profile.update');
 
 
     // Collection Show
-    Route::get('/', [CollectionController::class, 'index'])->name('collections.index');
-    Route::get('/collections/{collectionName}', [CollectionController::class, 'show'])->name('collections.show');
+    Route::get('/generic', [CollectionController::class, 'index'])->name('collections.index');
+    Route::get('/generic/collections/{collectionName}', [CollectionController::class, 'show'])->name('collections.show');
 
     //Collection Data CRUD
-    Route::delete('/collections/{collectionName}/{id}', [CollectionController::class, 'destroy'])->name('collections.destroy');
-    Route::get('/collections/{collectionName}/{id}/edit', [CollectionController::class, 'edit'])->name('collections.edit');
-    Route::post('/collections/{collection}/{id}', [CollectionController::class, 'update'])->name('collections.update');
-    Route::post('/collections/{collectionName}/{id}/restore', [CollectionController::class, 'restore'])->name('collections.restore');
+    Route::delete('/generic/collections/{collectionName}/{id}', [CollectionController::class, 'destroy'])->name('collections.destroy');
+    Route::get('/generic/collections/{collectionName}/{id}/edit', [CollectionController::class, 'edit'])->name('collections.edit');
+    Route::post('/generic/collections/{collection}/{id}', [CollectionController::class, 'update'])->name('collections.update');
+    Route::post('/generic/collections/{collectionName}/{id}/restore', [CollectionController::class, 'restore'])->name('collections.restore');
 
 
     //Collection Data Upload
-    Route::post('/upload-data/{collectionName}', [CollectionController::class, 'bulkUpload'])->name('collection.upload');
+    Route::post('/generic/upload-data/{collectionName}', [CollectionController::class, 'bulkUpload'])->name('collection.upload');
    
     // Collection Download
-    Route::get('/collections/{collectionName}/download-template', [CollectionController::class, 'download'])->name('collections.downloadTemplate')->defaults('includeData', false);
-    Route::get('/collections/{collectionName}/download-data', [CollectionController::class, 'download'])->name('collections.downloadData')->defaults('includeData', true);
+    Route::get('/generic/collections/{collectionName}/download-template', [CollectionController::class, 'download'])->name('collections.downloadTemplate')->defaults('includeData', false);
+    Route::get('/generic/collections/{collectionName}/download-data', [CollectionController::class, 'download'])->name('collections.downloadData')->defaults('includeData', true);
 
     // Collection Creation 
-    Route::get('/collection-create', [CollectionController::class, 'create'])->name('collections.create');
-    Route::post('collection-store', [CollectionController::class, 'store'])->name('collections.store');
+    Route::get('/generic/collection-create', [CollectionController::class, 'create'])->name('collections.create');
+    Route::post('/generic/collection-store', [CollectionController::class, 'store'])->name('collections.store');
 
     // Activity , Suggesions
-    Route::get('/activity-logs', [CollectionController::class, 'activityLogs'])->name('activity.logs');
-    Route::get('/suggestions', [CollectionController::class, 'suggestCollections'])->name('collections.suggestions');
+    Route::get('/generic/activity-logs', [CollectionController::class, 'activityLogs'])->name('activity.logs');
+    Route::get('/generic/suggestions', [CollectionController::class, 'suggestCollections'])->name('collections.suggestions');
 
-    Route::get('admin/users', [AuthController::class, 'showUsers'])->name('admin.users');
-    Route::get('admin/users/{id}/edit', [AuthController::class, 'editUser'])->name('users.edit');
-    Route::put('admin/users/{id}', [AuthController::class, 'updateUser'])->name('users.update');
-    Route::delete('admin/users/{id}', [AuthController::class, 'destroyUser'])->name('users.destroy');
-    Route::get('admin/users/{id}/logs', [AuthController::class, 'viewActivityLogs'])->name('users.activityLogs');
+    Route::get('/generic/admin/users', [AuthController::class, 'showUsers'])->name('admin.users');
+    Route::get('/generic/admin/users/{id}/edit', [AuthController::class, 'editUser'])->name('users.edit');
+    Route::put('/generic/admin/users/{id}', [AuthController::class, 'updateUser'])->name('users.update');
+    Route::delete('/generic/admin/users/{id}', [AuthController::class, 'destroyUser'])->name('users.destroy');
+    Route::get('/generic/admin/users/{id}/logs', [AuthController::class, 'viewActivityLogs'])->name('users.activityLogs');
 
 
 
